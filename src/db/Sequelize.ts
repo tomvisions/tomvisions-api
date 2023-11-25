cd9721fd7927c01716d84c1b43d88fade890d21b
Tom <tomc@tomvisions.com>
2023-11-25 09:12:10 -0500

    updates

--- src/db/Sequelize.ts
+++ src/db/Sequelize.ts
@@ -37,6 +37,9 @@ export class SequelizeApi {
         this._username = username;
         this._password = password;
         this._options = options;
+
+        console.log('in here');
+        console.group(this);
     }
 
     /**
